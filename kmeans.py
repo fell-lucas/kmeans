a487f5de40bcb460a89374c99fadbbb380c4c725
Lucas Fell <lucasafell@gmail.com>
2020-11-26 21:44:50 -0300

    variable names, clusters and centroids to file

--- kmeans.py
+++ kmeans.py
@@ -4,13 +4,13 @@ from math import sqrt
 from time import process_time
 
 start = process_time()
-a = readtable("data2.dat")
-nClusters = 6
+a = readtable("data/dataset.txt")
+k = 6
 
 membros = []
-posCentroides = []
-for x in range(0, nClusters):
-  posCentroides.append([0.0,0.0]) # x, y
+centroids = []
+for x in range(0, k):
+  centroids.append([0.0,0.0]) # x, y
   membros.append(0)
 
 def dist(p1, p2):
@@ -18,32 +18,52 @@ def dist(p1, p2):
   return d
 
 for i in a:
-  i.append(randint(0, nClusters - 1))
+  i.append(randint(0, k - 1))
   membros[i[2]] = membros[i[2]] + 1
 
 for step in range(0, 1000):
   for i, x in enumerate(a):
-    posCentroides[x[2]][0] += x[0]
-    posCentroides[x[2]][1] += x[1]
+    centroids[x[2]][0] += x[0]
+    centroids[x[2]][1] += x[1]
 
-  for i in range(0, nClusters):
-    posCentroides[i][0] /= membros[i]
-    posCentroides[i][1] /= membros[i]
+  for i in range(0, k):
+    centroids[i][0] /= membros[i]
+    centroids[i][1] /= membros[i]
 
-  for ponto in a:
-    near = dist(ponto, posCentroides[ponto[2]])
-    for i, c in enumerate(posCentroides):
-      newnear = dist(ponto, c)
+  for point in a:
+    near = dist(point, centroids[point[2]])
+    for i, c in enumerate(centroids):
+      newnear = dist(point, c)
       if(newnear < near):
         near = newnear
-        ponto[2] = i
+        point[2] = i
 
 print(process_time() - start)
+
+a.sort(key = lambda a: a[2])
+open("data/clusters.txt", "w").close()
+f1 = open("data/clusters.txt", "a")
+for i, line in enumerate(a):
+  if(i == 0):
+    f1.write(str(line[0]) + " " + str(line[1]) + "\n")
+    continue
+  elif (a[i][2] != a[i-1][2]):
+    f1.write("\n\n")
+    f1.write(str(line[0]) + " " + str(line[1]) + "\n")
+    continue
+  f1.write(str(line[0]) + " " + str(line[1]) + "\n")
+f1.close()
+
+
 xC = []
 yC = []
-for c in posCentroides:
+open("data/centroids.txt", "w").close()
+f2 = open("data/centroids.txt", "a")
+for i, c in enumerate(centroids):
   xC.append(c[0])
   yC.append(c[1])
+  f2.write(str(c[0]) + " " + str(c[1]) + "\n\n\n")
+f2.close()
 
 plot(column(a, 0), column(a, 1), 'g+')
 plot(xC, yC, 'ro')
